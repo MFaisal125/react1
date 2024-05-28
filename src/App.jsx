4e53af107521c2bd07b18cee6d33fe70ae156094
MFaisal125 <mfaisal0125470@gmail.com>
2024-05-27 22:33:42 -0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { HashRouter, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import "./index.css";
 
 function App() {
   return (
     <>
-      <Header />
+      <HashRouter>
+        <Routes>
+          <Header />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
